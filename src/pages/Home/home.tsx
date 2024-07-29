092170e2fccf6e61203547a19d00f7657b762f30
Nirjoy Debnath <nirjoydebnath61@gmail.com>
2024-07-29 13:30:54 +0600

    Home page design implemented

--- src/pages/Home/home.tsx
+++ src/pages/Home/home.tsx
@@ -1,6 +1,7 @@
 import { IconSearch } from "@tabler/icons-react";
 import Header from "../../components/Header/header";
 import { getUserName } from "../../helpers/jwtHelper";
+import Card from "../../components/Card/card";
 
 const Home = () => {
   const userName = getUserName();
@@ -8,7 +9,7 @@ const Home = () => {
   return (
     <>
       <Header />
-      <div className="flex-grow flex flex-col">
+      <div className="flex-grow flex flex-col mb-10">
         <div className="flex justify-center py-5 border-b-2 mx-10">
           <div className="flex rounded-md border border-black focus-within:ring-1 focus-within:ring-black focus-within:border-none w-1/2">
             <input
@@ -23,6 +24,93 @@ const Home = () => {
               {<IconSearch />}
             </button>
           </div>
+        </div>
+        {/* <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 place-items-center"> */}
+        <div className="grid grid-cols-[repeat(auto-fit,_minmax(384px,_1fr))] gap-4 place-items-center mt-5">
+          <Card
+            title="Title"
+            userName="UserName"
+            blog="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+            like={10}
+            comment={10}
+          />
+          <Card
+            title="Title"
+            userName="UserName"
+            blog="Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci error impedit esse aliquid illum velit animi. Omnis hic excepturi sunt, enim accusantium facilis distinctio dolor incidunt fugiat soluta rerum eum!"
+            like={10}
+            comment={10}
+          />
+          <Card
+            title="Title"
+            userName="UserName"
+            blog="Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci error impedit esse aliquid illum velit animi. Omnis hic excepturi sunt, enim accusantium facilis distinctio dolor incidunt fugiat soluta rerum eum!"
+            like={10}
+            comment={10}
+          />
+          <Card
+            title="Title"
+            userName="UserName"
+            blog="Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci error impedit esse aliquid illum velit animi. Omnis hic excepturi sunt, enim accusantium facilis distinctio dolor incidunt fugiat soluta rerum eum!"
+            like={10}
+            comment={10}
+          />
+          <Card
+            title="Title"
+            userName="UserName"
+            blog="Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci error impedit esse aliquid illum velit animi. Omnis hic excepturi sunt, enim accusantium facilis distinctio dolor incidunt fugiat soluta rerum eum!"
+            like={10}
+            comment={10}
+          />
+          <Card
+            title="Title"
+            userName="UserName"
+            blog="Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci error impedit esse aliquid illum velit animi. Omnis hic excepturi sunt, enim accusantium facilis distinctio dolor incidunt fugiat soluta rerum eum!"
+            like={10}
+            comment={10}
+          />
+          <Card
+            title="Title"
+            userName="UserName"
+            blog="Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci error impedit esse aliquid illum velit animi. Omnis hic excepturi sunt, enim accusantium facilis distinctio dolor incidunt fugiat soluta rerum eum!"
+            like={10}
+            comment={10}
+          />
+          <Card
+            title="Title"
+            userName="UserName"
+            blog="Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci error impedit esse aliquid illum velit animi. Omnis hic excepturi sunt, enim accusantium facilis distinctio dolor incidunt fugiat soluta rerum eum!"
+            like={10}
+            comment={10}
+          />
+          <Card
+            title="Title"
+            userName="UserName"
+            blog="Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci error impedit esse aliquid illum velit animi. Omnis hic excepturi sunt, enim accusantium facilis distinctio dolor incidunt fugiat soluta rerum eum!"
+            like={10}
+            comment={10}
+          />
+          <Card
+            title="Title"
+            userName="UserName"
+            blog="Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci error impedit esse aliquid illum velit animi. Omnis hic excepturi sunt, enim accusantium facilis distinctio dolor incidunt fugiat soluta rerum eum!"
+            like={10}
+            comment={10}
+          />
+          <Card
+            title="Title"
+            userName="UserName"
+            blog="Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci error impedit esse aliquid illum velit animi. Omnis hic excepturi sunt, enim accusantium facilis distinctio dolor incidunt fugiat soluta rerum eum!"
+            like={10}
+            comment={10}
+          />
+          <Card
+            title="Title"
+            userName="UserName"
+            blog="Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci error impedit esse aliquid illum velit animi. Omnis hic excepturi sunt, enim accusantium facilis distinctio dolor incidunt fugiat soluta rerum eum!"
+            like={10}
+            comment={10}
+          />
         </div>
       </div>
     </>
